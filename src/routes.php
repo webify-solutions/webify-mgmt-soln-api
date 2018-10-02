1e7fc12ca8c97349df297abb574a6c1fad858ed6
Mohammed Waked <mohammed.waked@aircanada.ca>
2018-10-02 09:46:34 +0300

    Added api html docs

--- src/routes.php
+++ src/routes.php
@@ -16,10 +16,58 @@ $app->get(str_replace("{base_path}", base_path, "{base_path}/status"), function 
   return $response->withJson($message);
 });
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->post(str_replace("{base_path}", base_path, "{base_path}/login"), function (Request $request, Response $response, array $args) {
+  $this->logger->info("Requesting login");
+
+  return $response->withStatus(501);
+});
+
+$app->get(str_replace("{base_path}", base_path, "{base_path}/logout"), function (Request $request, Response $response, array $args) {
+  $this->logger->info("Requesting logout");
+
+  return $response->withStatus(501);
+});
+
+$app->get(str_replace("{base_path}", base_path, "{base_path}/customers"), function (Request $request, Response $response, array $args) {
+  $this->logger->info("Requesting customers");
+
+  return $response->withStatus(501);
+});
+
+$app->get(str_replace("{base_path}", base_path, "{base_path}/products"), function (Request $request, Response $response, array $args) {
+  $this->logger->info("Requesting products");
+
+  return $response->withStatus(501);
+});
+
+$app->get(str_replace("{base_path}", base_path, "{base_path}/technicians"), function (Request $request, Response $response, array $args) {
+  $this->logger->info("Requesting technicians");
+
+  return $response->withStatus(501);
+});
+
+$app->get(str_replace("{base_path}", base_path, "{base_path}/issues"), function (Request $request, Response $response, array $args) {
+  $this->logger->info("Requesting issues");
+
+  return $response->withStatus(501);
+});
+
+$app->post(str_replace("{base_path}", base_path, "{base_path}/issues"), function (Request $request, Response $response, array $args) {
+  $this->logger->info("Posting issue");
+
+  return $response->withStatus(501);
+});
+
+$app->patch(str_replace("{base_path}", base_path, "{base_path}/issues/{issue_id}"), function (Request $request, Response $response, array $args) {
+  $this->logger->info(str_replace("{issue_id}", $args['issue_id'], "Patching issue {issue_id}"));
+
+  return $response->withStatus(501);
+});
+
+$app->get(str_replace("{base_path}", base_path, "{base_path}"), function (Request $request, Response $response, array $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    $this->logger->info("Requesting API definition");
 
     // Render index view
-    return $this->renderer->render($response, "index.phtml", $args);
+    return $this->renderer->render($response, "index.html", $args);
 });
