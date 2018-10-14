ab2f1e31999ae83edb7e92da1bd3602df9c1d67d
Mohammed Waked <mohammed.waked@webify-solutions.com>
2018-10-14 11:20:11 +0300

    Expose response header

--- public/index.php
+++ public/index.php
@@ -72,7 +72,8 @@ if ($dh = opendir($dir))
 }
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: X-Token, X-User-Role');
+header('Access-Control-Allow-Headers: X-Token');
+header('Access-Control-Exponse-Headers: X-User-Role');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH');
 
 // Run app
